0bbedeefa70500e9c158883e4cde6d2a6ee921fb
janeljs <jisunlim818@gmail.com>
2021-07-10 15:38:16 +0900

    feat: JPA Auditing 추가

--- group15/src/main/java/com/gdg/group15/Group15Application.java
+++ group15/src/main/java/com/gdg/group15/Group15Application.java
@@ -2,7 +2,9 @@ package com.gdg.group15;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Group15Application {
 
