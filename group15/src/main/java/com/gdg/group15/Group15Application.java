3ce0da4dfcc5353add5d7577514162a9cf1afdaf
janeljs <jisunlim818@gmail.com>
2021-07-10 11:41:34 +0900

    chore: 스프링 프로젝트 설정

---
+++ group15/src/main/java/com/gdg/group15/Group15Application.java
@@ -0,0 +1,13 @@
+package com.gdg.group15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group15Application.class, args);
+	}
+
+}
---
+++ group15/src/test/java/com/gdg/group15/Group15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.group15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
